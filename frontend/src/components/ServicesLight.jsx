f4e7f54285cebcbc0a4ddb1628044bffe300d505
arpit <arpitgurudev@gmailcom>
2026-06-30 17:05:22 +0530

    manual receipt generator

--- frontend/src/components/ServicesLight.jsx
+++ frontend/src/components/ServicesLight.jsx
@@ -75,7 +75,7 @@ export const ServicesLight = () => {
     } else if (service.title.includes("Aura")) {
       imageSrc = process.env.PUBLIC_URL + '/assets/service-aura.png';
     } else {
-      imageSrc = process.env.PUBLIC_URL + '/assets/uploads/img4.png';
+      imageSrc = process.env.PUBLIC_URL + '/assets/livereading.jpeg';
     }
 
     return (
